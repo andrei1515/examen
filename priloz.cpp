495af4948452bb6797b085e3ecf841303fce570b
cute <157357901+andrei1515@users.noreply.github.com>
2025-02-25 10:34:47 +0300

    Add files via upload

---
+++ priloz.cpp
@@ -0,0 +1,29 @@
+﻿﻿//библиотеки
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+
+	int x;
+	float y, k;
+
+	printf("Vvedite celoe polozhitelnoe chislo x: ");
+
+	do //проверочка на то, что число положительное
+	{
+		scanf("%d", &x);
+		if (x <= 0)
+		{
+			printf("Error. Vvedite celoe polozhitelnoe chislo x: ");
+		}
+	} while (x <= 0);
+
+	y = sqrt(pow(2, x));
+
+	printf("Result = %f", y);
+
+
+
+	return 0;
+}
